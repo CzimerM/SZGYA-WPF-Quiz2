c8904d2b9948423b5b1da3d7296e99f66a5e2d6d
Csizmadia Márk <kindmysticaltree@gmail.com>
2024-03-11 14:04:39 +0100

    Add project files.

---
+++ SZGYA-WPF-Quiz2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SZGYA_WPF_Quiz2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
